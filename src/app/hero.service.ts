5d95bbc3edfc80e806c316f0d5549134782e72a4
jsadler <jsadler@spear.qedsysinc.com>
2017-10-31 13:06:11 -0400

    Additional crud methods

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,25 +7,26 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService{
-    private headers = new Headers({'Content-Type': 'application/json'});
     private heroesUrl = 'api/heroes';
 
     constructor(private http: Http){}
 
     getHeroes(): Promise<Hero[]> {
+        
         return this.http.get(this.heroesUrl)
             .toPromise()
-            .then(response => alert (response)) //response => response.json().heroes as Hero[])
+            .then(response => response.json().data as Hero[])
             .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error);
+        console.log(error);
         return Promise.reject(error.message || error);
     }
 
     getHero(id: number): Promise<Hero> { 
-        const url = '${this.heroesUrl}/${id}';
+        const url = `${this.heroesUrl}/${id}`;
         return this.http.get(url)
         .toPromise()
         .then(response => response.json().Heroes as Hero)
